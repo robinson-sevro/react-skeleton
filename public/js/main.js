11dcc9818a9eaa399e6f60fb6fcda373725328c1
Robinson Hernandez <robinson@sevro.co>
2022-01-22 06:51:49 +0800

    pre alpha commits - not compilable yet due to babel issues

---
+++ public/js/main.js
@@ -0,0 +1,1 @@
+console.error("Error: Plugin/Preset files are not allowed to export objects, only functions. In C:\\Users\\robun\\PROJECTS\\sevro\\react-training\\react-skeleton\\node_modules\\react\\index.js while parsing file: C:\\Users\\robun\\PROJECTS\\sevro\\react-training\\react-skeleton\\src\\main.jsx");
